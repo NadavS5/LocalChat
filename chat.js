dbb9d751d0199c15afc96e30fc27bc6547ef2460
nadavs5 <electric5008@gmail.com>
2025-10-15 18:37:50 +0300

    feat: functioning chat, next is to show the messages visualy

--- chat.js
+++ chat.js
@@ -1,18 +1,23 @@
 import {addProgressItem, setProgress, toggleProgress} from './progress.js'
 
-console.log("hello")
+console.log("welcome to LocalChat")
 // import {pipeline} from "https://cdn.jsdelivr.net/npm/@huggingface/transformers";
 
 // console.log(pipeline)
 const prompt_input = document.querySelector("#prompt_input")
 const load_button = document.querySelector("#load_btn")
 
+let message_history = [
+        {"role": "system", "content": "You are a friendly chatbot who always responds in the style of a pirate",},
+]
+let generating = false
 
 const worker= new Worker("/worker.js", {
     type: "module",
 });
 
 let modelLoading = false
+let current_message = ""
 // future prep for ts
 // function onMessageReceived(message: MessageEvent)
 function onMessageReceived(message) {
@@ -30,14 +35,11 @@ function onMessageReceived(message) {
         case "ready":
             // console.log("model ready!")
             prompt_input.removeAttribute("disabled")
-            load_button.style.display = "none"
+            load_button.style.display = "none"  
             toggleProgress()
             console.log("ready")
-            const messages = [
-                {"role": "system", "content": "You are a friendly chatbot who always responds in the style of a pirate",},
-                {"role": "user", "content": "write me a basic python code"},
-            ]   
-            worker.postMessage({type: "generate", data: messages})
+            
+            
             break;
         case "loading":
             modelLoading = true
@@ -45,10 +47,14 @@ function onMessageReceived(message) {
             break;
         case "complete":
             //toggle send button
+            generating = false
+            message_history.push({"role": "asistant", "content": current_message})
+            current_message = ""
             break;
         case "update":
             // message.data.output contains numtokens and tps
             console.log(message.data.output)
+            current_message += message.data.output
             break;
     }
 }
@@ -57,6 +63,19 @@ function loadModel(e) {
     worker.postMessage({type: "load"})
     toggleProgress()
 }
+
+
+
+function send_message(event) {
+    if(!generating) {
+        message_history.push({"role": "user", "content" : document.querySelector("#prompt_input").value})
+        worker.postMessage({type: "generate", data: message_history})
+        generating = true
+    }
+}
+document.addEventListener("keydown", (e) => {if(e.key === 'Enter') send_message(e)});
+
+
 
 load_button.addEventListener("click", loadModel)
 
